0691bfc0885d9ad425cc6aab53f6c0d36a593ef0
TobyChen12138 <47099644+TobyChen12138@users.noreply.github.com>
2021-11-11 10:46:32 +0800

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,26 @@
+import falcon
+from falcon import uri
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+
+
+class Connect(object):
+
+    def on_get(self, req, resp):
+        query_string = req.query_string
+        query_list = query_string.split('&')
+        b = {}
+        for i in query_list:
+            b[i.split('=')[0]] = i.split('=')[1]
+
+        try:
+            check_signature(token='1qq7O213', signature=b['signature'], timestamp=b['timestamp'], nonce=b['nonce'])
+            resp.body = (b['echostr'])
+        except InvalidSignatureException:
+            pass
+        resp.status = falcon.HTTP_200
+
+
+app = falcon.API()
+connect = Connect()
+app.add_route('/connect', connect)
